fc38f30d44aa02a74a6203f63735bcd30577a831
ElkanRoelen <roelen@elkan.nl>
2019-04-13 12:31:40 +0200

    Updated deployment

--- Gates/build.sh
+++ Gates/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+$(aws ecr get-login --no-include-email --region eu-west-1)
+
+npm i
 ionic build --prod
 
-docker build -t smilo/klm-gate .
+docker build -t klm/gate:latest -t klm/gate:latest .
+
+# Tag for upload to Amazon
+docker tag klm/gate:latest 462619610638.dkr.ecr.eu-west-1.amazonaws.com/klm/gate:latest
+
+# Upload to amazon
+docker push 462619610638.dkr.ecr.eu-west-1.amazonaws.com/klm/gate:latest
